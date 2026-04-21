99537299369cd2b3ef072f60aea99cb046138c63
Ankita meena <104414458+AnkitaRaj2003@users.noreply.github.com>
2026-04-21 11:06:53 +0530

    Update score.py

--- Azure AI ML/Pretrained model/score.py
+++ Azure AI ML/Pretrained model/score.py
@@ -1,12 +1,21 @@
 import json
 import joblib
 import os
+import tarfile
 import numpy as np
 
 def init():
     global model
-    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model.pkl")
-    model = joblib.load(model_path)
+    
+    model_dir = os.getenv("AZUREML_MODEL_DIR")
+    tar_path = os.path.join(model_dir, "model.tar.gz")
+
+    # extract tar.gz
+    with tarfile.open(tar_path) as tar:
+        tar.extractall(model_dir)
+
+    # load extracted model file
+    model = joblib.load(os.path.join(model_dir, "model.pkl"))
 
 def run(raw_data):
     data = json.loads(raw_data)
